cc2eefa383fb2cad84ef9966f99f850adb45761b
Andrew Betts <andrew@trib.tv>
2018-02-27 11:21:40 +0000

    Add nosniff cases with sniffing enabled

--- fetch/nosniff/resources/worker.py
+++ fetch/nosniff/resources/worker.py
@@ -1,5 +1,6 @@
 def main(request, response):
     type = request.GET.first("type", None)
+    emitnosniff = request.GET.first("nosniff", None)
 
     content = "// nothing to see here"
     content += "\n"
@@ -7,8 +8,9 @@ def main(request, response):
 
     response.add_required_headers = False
     response.writer.write_status(200)
-    response.writer.write_header("x-content-type-options", "nosniff")
     response.writer.write_header("content-length", len(content))
+    if(emitnosniff != None):
+      response.writer.write_header("x-content-type-options", "nosniff")
     if(type != None):
       response.writer.write_header("content-type", type)
     response.writer.end_headers()
